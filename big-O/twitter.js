8611027dc716cfb22189b55cc2ab1e8a07abe8f6
Dean <dean@deansantillan.com>
2022-01-08 21:42:24 +1000

    task(): added more info on big o to the readme file.

---
+++ big-O/twitter.js
@@ -0,0 +1,5 @@
+// find 1st and the last item
+// grab all the tweets
+const array = ['tweet 1', 'tweet 2', 'tweet 3', 'tweet 4', 'tweet 5'];
+console.log(array[0]); // 0(1)
+console.log(array[array.length - 1]); // 0(1)
